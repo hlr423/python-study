c320e120df8e2d90d229e79aa1f075ce02ce0734
黄利蓉 <huanglirong_fh@12530.com>
2020-07-03 16:18:36 +0800

    commit

---
+++ dome.py
@@ -0,0 +1,12 @@
+print("==========gaogao============")
+a=input('又在看电视:')
+b=int(a)
+print(type(b))
+
+if b == 8:
+    print('又在看电视')
+    print('我猜对了吧')
+else:
+    print("猜错了，高高在很认真的学校")
+
+print('游戏结束，不玩了')
